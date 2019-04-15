b3f4c2af764935256a7d62476262baa5cec93cdb
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2019-04-15 08:35:20 +0200

    Add project files.

---
+++ XamlCreateFromStringSample/Controls/CustomerDetailControl.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using XamlCreateFromStringSample.Models;
+
+namespace XamlCreateFromStringSample.Controls
+{
+  public sealed partial class CustomerDetailControl : UserControl
+  {
+    public CustomerDetailControl()
+    {
+      this.InitializeComponent();
+    }
+
+    public Customer Customer
+    {
+      get { return (Customer)GetValue(CustomerProperty); }
+      set { SetValue(CustomerProperty, value); }
+    }
+
+    public static readonly DependencyProperty CustomerProperty =
+        DependencyProperty.Register("Customer", typeof(Customer), typeof(CustomerDetailControl), new PropertyMetadata(null));
+  }
+}
---
+++ XamlCreateFromStringSample/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Windows.Foundation.Metadata;
+
+namespace XamlCreateFromStringSample.Models
+{
+  [CreateFromString(MethodName = "XamlCreateFromStringSample.Models.Customer.CreateFromString")]
+  public class Customer
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public static Customer CreateFromString(string customerString)
+    {
+      var array = customerString.Split(' ');
+      return new Customer
+      {
+        FirstName = array[0],
+        LastName = array[1]
+      };
+    }
+  }
+}
